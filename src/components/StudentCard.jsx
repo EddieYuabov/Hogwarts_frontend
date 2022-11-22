8199a9935fb682cede8241af8a4fba2cb48cabd1
Aaron Sanchez <saarong12@gmail.com>
2022-11-21 21:24:44 -0600

    Added house icon

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 
 const StudentCard = ({name, house}) => {
+
+let image
+
+switch (house) {
+  case 'Gryffindor':
+    image = <img src='https://i.imgur.com/bRS19Ph.png' alt='house-icon'/>    
+    break;
+  case 'Slytherin':
+    image = <img src="https://i.imgur.com/smDpCz3.png" alt='house-icon'/>
+    break;
+  case 'Hufflepuff':
+    image = <img src="https://i.imgur.com/DEjIZf9.png" alt='house-icon'/>
+    break;
+  case 'Ravenclaw':
+    image = <img src="https://i.imgur.com/3LOX43O.png" alt='house-icon'/>
+    break;
+  default:
+    break;
+}
+
+
+
   return (
     <div className="cardDiv">
         <h5 className="studentCardName">{name}</h5>
         <h5 className="studentCardHouse">{house}</h5>    
+        {image}
     </div>
   )
 }

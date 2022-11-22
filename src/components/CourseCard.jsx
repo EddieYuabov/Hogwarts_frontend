46f99af9911f46f3f6b3f1fad6fb8d3bce56e5ee
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-11-22 10:45:28 -0500

    further style

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -52,9 +52,10 @@ const CourseCard = ({name, professor, grade}) => {
 
   return (
     <div className='courseCardContainer'>
-        <h5 className='courseName'>{name}</h5>
+        {/* <h5 className='studentCourseGrade'>{gradeLetter}</h5> */}
+        <h5 className='courseName'>{name} <span className='studentCourseGrade'>{gradeLetter}</span></h5>
         <h5 className='courseProf'>{professor}</h5>  
-        <h5 className='studentCourseGrade'>{gradeLetter}</h5>
+
         {image}
     </div>
   )
